561f938234733010e432b02355864a17d57f3b84
Minsetr0 <nesterovmaksim71@mail.ru>
2023-01-31 20:30:05 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Random rand = new Random();
+int number = rand.Next(0, 101);
+for (float i = 0.0f; i <= number; i++)
+{
+    float divisionIntoFive = i % 5;
+    float divisionIntoThree = i % 3;
+
+    if (divisionIntoFive == 0)
+    {
+        Console.WriteLine(i);
+    }
+    else if (divisionIntoThree == 0)
+    {
+        Console.WriteLine(i);
+    }
+}   
