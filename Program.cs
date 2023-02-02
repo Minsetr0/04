b74c5da7b11dabf8cd1c88ea6b5de594287b3cae
Minsetr0 <nesterovmaksim71@mail.ru>
2023-02-02 21:08:30 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,17 +1,25 @@
 ﻿Random rand = new Random();
 int number = rand.Next(0, 101);
+int sum = 0;
+int multiplicityCheck1 = 5;
+int multiplicityCheck2 = 3;
 
-for (float i = 0.0f; i <= number; i++)
+for (int i = 1; i <= number; i++)
 {
-    float divisionIntoFive = i % 5;
-    float divisionIntoThree = i % 3;
+    int divisionIntoFive = i % multiplicityCheck1;
+    int divisionIntoThree = i % multiplicityCheck2;
 
     if (divisionIntoFive == 0)
     {
         Console.WriteLine(i);
+        sum += i;
+
     }
     else if (divisionIntoThree == 0)
     {
         Console.WriteLine(i);
+        sum += i;
     }
 }
+
+Console.WriteLine("Сумма всех чисел, содержащихся в " + number + " и кратных '3' и '5' - " + sum + ".");
